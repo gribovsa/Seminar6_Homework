b49c2ce2da073d6ccb652955611cca8b468764de
Gribov Sergey <gribovsa@list.ru>
2024-01-27 16:40:38 +0300

    Решил задачу2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 2: Задайте строку, содержащую латинские буквы 
+в обоих регистрах. Сформируйте строку, в которой все 
+заглавные буквы заменены на строчные. 
+
+“aBcD1ef!-” => “abcd1ef!-” 
+*/
+
+string GetStringToLower(string inString)
+{
+    string outString = inString.ToLower();
+    return outString;
+}
+
+Console.Write("Введите строку: ");
+string str = Console.ReadLine();
+string res = GetStringToLower(str);
+Console.WriteLine($"Результат - {res}");
